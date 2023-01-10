5868717fe4e6bd34110f401c6880ad6c249b1a23
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2023-01-10 17:13:53 -0500

    Clean up __init__

--- glue_single_cell/__init__.py
+++ glue_single_cell/__init__.py
@@ -4,7 +4,6 @@ def setup():
     from .menubar_plugin import diff_gene_exp_plugin  # noqa
     from .menubar_plugin import pca_subset_exp_plugin  # noqa
     from .menubar_plugin import enrichrpy_plugin # noqa
-    from .menubar_plugin import uscd_genome_plugin # noqa
     from .qtl_viewer.viewer import QTLViewer # noqa
 
     from glue.config import qt_client
