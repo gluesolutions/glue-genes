0a06f7b6344ebad349fb3b5b5833c2fc1eef64d3
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2023-09-05 12:23:55 -0400

    Fix formatting

--- glue_genes/glue_genomics_data/geojson_factory.py
+++ glue_genes/glue_genomics_data/geojson_factory.py
@@ -3,15 +3,13 @@ A simple geojson parser for glue that does not require geopandas.
 This is designed to work primarily with QuPath annotation files.
 
 QuPath GeoJSON defines (0,0) as the upper left corner of the image,
-while glue defines (0,0) as the lower left corner of the image. 
+while glue defines (0,0) as the lower left corner of the image.
 
 We need to tackle the transformation of ExtendedComponents
 in order to deal with this.
 
 We cannot fix this in the data loader as there is no reference
 to the size of the image (which would be necessary to do the transform).
-
-
 """
 
 import json
@@ -22,9 +20,9 @@ from glue.core.component import ExtendedComponent
 from glue.config import data_factory
 
 
-
 def is_geojson(filename):
-    return filename.endswith('.geojson')
+    return filename.endswith(".geojson")
+
 
 @data_factory("GeoJSON data loader", is_geojson, priority=100)
 def read_geojson(filename):
@@ -35,15 +33,15 @@ def read_geojson(filename):
     properties = []
     center_x = []
     center_y = []
-    for feature in features['features']:
-        geom = shape(feature['geometry'])
+    for feature in features["features"]:
+        geom = shape(feature["geometry"])
         rep_point = geom.point_on_surface()
         center_x.append(rep_point.x)
         center_y.append(rep_point.y)
         geometries.append(geom)
-        properties.append(feature['properties'])
-    data.add_component(center_x,'x')
-    data.add_component(center_y,'y')
+        properties.append(feature["properties"])
+    data.add_component(center_x, "x")
+    data.add_component(center_y, "y")
 
     geom_comp = ExtendedComponent(
         np.asarray(geometries),
@@ -54,13 +52,14 @@ def read_geojson(filename):
     )
     data.add_component(geom_comp, label="regions")
 
-
     property_keys = {k for d in properties for k in d}
     for prop_key in property_keys:
-        new_col = [x.get(prop_key,None) for x in properties]
+        new_col = [x.get(prop_key, None) for x in properties]
         try:
-            data.add_component(new_col,prop_key)
+            data.add_component(new_col, prop_key)
         except ValueError as e:
-            print(f"The property {prop_key} was not added to the Data object because: {e}")
+            print(
+                f"The property {prop_key} was not added to the Data object because: {e}"
+            )
             pass
     return data
--- glue_genes/glue_genomics_data/image_link_functions.py
+++ glue_genes/glue_genomics_data/image_link_functions.py
@@ -1,11 +1,19 @@
-from glue.core.link_helpers import LinkTwoWay, MultiLink
-from glue.core.link_helpers import link_helper
-from glue.core.link_helpers import BaseMultiLink
+from glue.core.link_helpers import (
+    LinkTwoWay,
+    MultiLink,
+    LinkSame,
+    link_helper,
+    BaseMultiLink,
+)
+
 
 class BaseLinearScaleLink(BaseMultiLink):
     """
     This class can be used to simply add a scale and shift
     transform between (e.g.) images and annotations as:
+
+        from glue.config import link_helper
+        from glue_genes.glue_genomics_data.image_link_functions import BaseLinearScaleLink
 
         @link_helper(category='Images')
         class Fullres_to_Hires(BaseLinearScaleLink):
@@ -15,8 +23,8 @@ class BaseLinearScaleLink(BaseMultiLink):
             display = "Fullres <-> HiRes"
             scale = 0.053972367
             shift = 0
-
     """
+
     def forwards(self, input):
         return self.scale * input + self.shift
 
@@ -26,11 +34,12 @@ class BaseLinearScaleLink(BaseMultiLink):
 
 @link_helper(category="Images")
 class FlipLink(MultiLink):
-
     display = "Flip y axis"
-    description = ("Join two components while flipping the y axis. This"
-                   "is sometimes necessary when image coordinates are"
-                   "specified with (0,0) in the upper left corner.")
+    description = (
+        "Join two components while flipping the y axis. This"
+        "is sometimes necessary when image coordinates are"
+        "specified with (0,0) in the upper left corner."
+    )
 
     labels1 = ["Image x coords", "Image y coords"]
     labels2 = ["Region/annotation x coords", "Region/annotation y coords"]
@@ -38,6 +47,7 @@ class FlipLink(MultiLink):
     def __init__(self, *args, cids1=None, cids2=None, data1=None, data2=None):
         self.data1 = data1
         image_height, _ = data1.shape
+
         def flip_y(coords):
             return image_height - coords
 
