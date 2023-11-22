dcd59774b6882570666e8eea6cf879eec0c19f7c
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2023-11-22 14:45:02 -0500

    Make DGE more robust to changing subsets and increase tests (#58)

--- glue_genes/glue_single_cell/qt/tests/test_diff_gene_exp.py
+++ glue_genes/glue_single_cell/qt/tests/test_diff_gene_exp.py
@@ -6,9 +6,24 @@ from glue.core import data_factories as df
 
 from glue_genes.glue_single_cell.anndata_factory import read_anndata
 
-from ..diff_gene_exp import get_gene_diff_exp
+from ..diff_gene_exp import get_gene_diff_exp, DiffGeneExpDialog
 
 DATA = os.path.join(os.path.dirname(__file__), "data")
+
+
+# If SyncComponents kept their sync state, we could use this to check
+def has_insync_component(data):
+    for comp in data.main_components:
+        if 'sync' in comp.label:
+            return True
+    return False
+
+
+def has_outofsync_component(data):
+    for comp in data.main_components:
+        if 'out-of-sync' in comp.label:
+            return True
+    return False
 
 
 class TestDiffGeneExp(object):
@@ -42,6 +57,17 @@ class TestDiffGeneExp(object):
         self.subset_single = self.dc.new_subset_group(
             label="SinglePoint", subset_state=self.obs_data.id["Pixel Axis 0 [x]"] == 0
         )
+        # The first ten genes in the table
+        self.gene_subset = self.dc.new_subset_group(
+            label="Genes", subset_state=self.var_data.id["Pixel Axis 0 [x]"] < 10
+        )
+
+        # A subset of genes on the X array will be over all observations, but is
+        # not a valid subset of observations for differential gene expression
+        self.gene_subset_from_X_array = self.dc.new_subset_group(
+            label="Genes from X array", subset_state=self.dd.id["Pixel Axis 1 [x]"] < 10
+        )
+
         assert len(self.dd.meta['obs_data'].subsets[0]['cell_type']) == 15
         assert len(self.dd.meta['obs_data'].subsets[1]['cell_type']) == 48
         assert len(self.dd.meta['obs_data'].subsets[2]['cell_type']) == 1
@@ -65,9 +91,45 @@ class TestDiffGeneExp(object):
 
         assert df["pvals_adj"].max() > 0.95
 
-    def test_diff_gene_exp_fails(self):
-        with pytest.raises(ValueError):
+    def test_dge_fails_too_small(self):
+        with pytest.raises(ValueError, match='less than 2 observations'):
             _ = get_gene_diff_exp(self.subset_single, None, self.dd)
+
+    def test_dge_fails_bad_subset(self):
+        with pytest.raises(ValueError,  match='Failed to generate a mask over observations'):
+            _ = get_gene_diff_exp(self.gene_subset, None, self.dd)
+
+        with pytest.raises(ValueError, match='includes all the observations'):
+            _ = get_gene_diff_exp(self.gene_subset_from_X_array, None, self.dd)
+
+    def test_dge_fails_bad_subsets_two_subsets(self):
+        with pytest.raises(ValueError, match='Failed to generate a mask over observations'):
+            _ = get_gene_diff_exp(self.subset1, self.gene_subset, self.dd)
+
+        with pytest.raises(ValueError, match='less than 2 observations'):
+            _ = get_gene_diff_exp(self.subset_single, self.subset2, self.dd)
+
+    def test_full_with_dialog(self):
+
+        d = DiffGeneExpDialog(self.dc)
+        d.state.subset1 = self.subset1
+        d.state.subset2 = self.subset2
+        d._apply(show_dialog=False)
+        assert len(d.state.data.listeners) == 1
+        assert has_insync_component(self.var_data)
+
+        d.state.subset1.subset_state = self.obs_data.id["Pixel Axis 0 [x]"] == 0
+        assert has_outofsync_component(self.var_data)
+
+        d.state.subset1.subset_state = self.obs_data.id["cell_type"] == "B"
+
+        assert not has_outofsync_component(self.var_data)
+        assert has_insync_component(self.var_data)
+
+        for subset in d.state.subset1.subsets:
+            subset.delete()
+        assert len(d.state.data.listeners) == 0
+        assert not has_insync_component(self.var_data)
 
 
 class TestDiffGeneExpBacked(TestDiffGeneExp):
